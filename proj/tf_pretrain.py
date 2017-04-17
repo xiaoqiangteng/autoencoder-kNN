a1945dbcc23e50a7634b839c2f533f01c05ac6b2
zhan1182 <jz2776@columbia.edu>
2017-04-17 15:14:55 -0400

    tune para

--- proj/tf_pretrain.py
+++ proj/tf_pretrain.py
@@ -19,7 +19,7 @@ def cnn_nca_mnist_pretrain(trial, train_percentage=0.1, test_percentage=0.1):
 
     auto = Autoencoder()
 
-    learning_rate = 0.001
+    learning_rate = 0.01
 
     optimizer_rec_error = tf.train.AdamOptimizer(learning_rate).minimize(auto.reconstruction_error)
 
@@ -31,7 +31,7 @@ def cnn_nca_mnist_pretrain(trial, train_percentage=0.1, test_percentage=0.1):
 
     # Pre-train step
     batch_size = 100
-    epochs = 5
+    epochs = 50
     rec_error = np.inf
     for epoch_i in range(epochs):
         for batch_i in range(train_m // batch_size):
