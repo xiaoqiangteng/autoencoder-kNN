d43b57277741c4246ecc5ddb710560a29c8557bb
zhan1182 <jz2776@columbia.edu>
2017-04-17 22:16:33 -0400

    tune para

--- proj/tf_train.py
+++ proj/tf_train.py
@@ -44,7 +44,7 @@ def cnn_nca_mnist_train(trial, train_percentage=0.1, test_percentage=0.1):
 
 
     # Train step
-    batch_size = 4500
+    batch_size = 4250
     epochs = 60
     minimum_loss = np.inf
     for epoch_i in range(epochs):
