df08a760ffae0ee713d55114c61d0d3fd0332fe7
zhan1182 <jz2776@columbia.edu>
2017-04-20 13:41:44 -0400

    linear nca test

--- proj/nca_knn.py
+++ proj/nca_knn.py
@@ -7,54 +7,63 @@ from metric_learn import NCA, LMNN
 from knn import kNN
 
 def nca_mnist_experiment(trial, train_percentage=0.1, test_percentage=0.1):
-    knn = kNN()
-    knn.load_mnist_data(train_percentage, test_percentage)
 
-    print(knn.x.shape)
+    encoding_train_imgs_path = './data/MNIST_encoding/tf_train.encoding'
+    encoding_test_imgs_path = './data/MNIST_encoding/tf_test.encoding'
+
+    train_labels_path = './data/MNIST_encoding/tf_train.labels'
+    test_labels_path = './data/MNIST_encoding/tf_test.labels'
+
+    encoding_train = pickle.load(open(encoding_train_imgs_path, 'rb'))
+    encoding_test = pickle.load(open(encoding_test_imgs_path, 'rb'))
+
+    print(encoding_train.shape)
+
+    train_labels = pickle.load(open(train_labels_path, 'rb'))
+    test_labels = pickle.load(open(test_labels_path, 'rb'))
+
+    print(train_labels.shape)
+
+    m = len(encoding_train)
+    train_m = int(m * train_percentage)
+    sel = random.sample(range(m), train_m)
+    X = encoding_train.astype(np.float)[sel]
+    y = train_labels[sel]
+
+    print(X.shape)
+    print(y.shape)
+
+    m = len(encoding_test)
+    test_m = int(m * test_percentage)
+    sel = random.sample(range(m), test_m)
+
+    X_test = encoding_test.astype(np.float)[sel]
+    y_test = test_labels[sel]
+
+    print(X_test.shape)
+    print(y_test.shape)
 
     nca = NCA(max_iter=100, learning_rate=0.01)
-
-    # sx = csr_matrix(knn.x[:, :32])
-    sx = knn.x[:, :32]
-
-    nca.fit(sx, knn.y)
-
+    nca.fit(X, y)
     x_train = nca.transform()
+    x_test = nca.transform(X_test)
 
-    # sx = csr_matrix(knn.x_test[:, :32])
-    sx = knn.x_test[:, :32]
+    knn = kNN()
+    k_valus = [1, 3, 5, 7]
+    for k in k_valus:
+        knn.k = k
 
-    x_test = nca.transform(sx)
+        acc_list = []
+        for _ in range(trial):
+            acc = knn.evaluate(x_train, y, x_test, y_test)
+            acc_list.append(acc)
 
-    print(x_train.shape)
-    print(x_test.shape)
-
-    # lmnn = LMNN(k=5, learn_rate=1e-6)
-    # lmnn.fit(knn.x[:, :30], knn.y)
-
-    # x_train = lmnn.transform()
-    # x_test = lmnn.transform(knn.x_test[:, :30])
-
-    # print(x_train.shape)
-    # print(x_test.shape)
-
-
-    # k_valus = [1, 3, 5, 7]
-    # for k in k_valus:
-    #     knn.k = k
-
-    #     acc_list = []
-    #     for _ in range(trial):
-    #         acc = knn.evaluate(knn.x, knn.y, knn.x_test, knn.y_test)
-    #         acc_list.append(acc)
-
-    #     print(np.mean(np.array(acc_list)))
-
+        print(np.mean(np.array(acc_list)))
 
 
 def main():
-    train_percentage = 0.05
-    test_percentage = 0.05
+    train_percentage = 0.1
+    test_percentage = 0.1
     trial = 1
 
     nca_mnist_experiment(trial, train_percentage, test_percentage)
